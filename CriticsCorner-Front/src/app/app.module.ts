c686e0183076d6d794fe7a49f052f1c98ab08788
adrianpop3 <adrian.pop2@student.upt.ro>
2023-04-10 16:48:17 +0300

    Routes and link between Login & Register created

--- CriticsCorner-Front/src/app/app.module.ts
+++ CriticsCorner-Front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import{ HttpClientModule} from '@angular/common/http'
 import { LoginComponent } from './components/login/login.component'
@@ -11,7 +12,6 @@ import { LoginComponent } from './components/login/login.component'
   declarations: [
     AppComponent,
     RegistrationComponent,
-    //LoginComponent,
     LoginComponent
   ],
   imports: [
