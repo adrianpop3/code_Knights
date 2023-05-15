4ad92ec0a3be4b1743ce25bc631be2ba09fede52
adrianpop3 <adrian.pop2@student.upt.ro>
2023-05-15 18:38:26 +0300

    Unit Tests for services done

--- CriticsCorner-Front/src/app/services/registration.service.spec.ts
+++ CriticsCorner-Front/src/app/services/registration.service.spec.ts
@@ -1,16 +1,53 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { RegistrationService } from './registration.service';
+import { User } from 'app/classes/user';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ToastrService } from 'ngx-toastr';
 
-describe('RegistrationService', () => {
+fdescribe('RegistrationService', () => {
   let service: RegistrationService;
-
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  let toastrSpy: any;
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: ToastrService, useValue: toastrSpy }],
+    });
+
+    injector = getTestBed();
+    httpMock = injector.inject(HttpTestingController);
+    toastrSpy = jasmine.createSpyObj('ToastrService', ['success', 'error']);
     service = TestBed.inject(RegistrationService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#registerUser()', () => {
+    it('should return an Observable<User>', () => {
+      const dummyUser: User = {
+        id: 1,
+        username: 'testun',
+        password: 'passun',
+        confirmpassword: 'passun',
+        email: 'test@email.com',
+      }
+
+      service
+        .registerUser(dummyUser)
+        .subscribe((user) => {
+          expect(user).toEqual(dummyUser);
+        });
+
+      const req = httpMock.expectOne(
+        `${service.baseUrl}`
+      );
+      expect(req.request.method).toBe('POST');
+      req.flush(dummyUser);
+    });
   });
 });
